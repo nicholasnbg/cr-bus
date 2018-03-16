55e0aefc3d106975c7e1850143a48761268da4ef
nicholasnbg <nicholasnbg@gmail.com>
2018-03-16 18:27:34 +0900

    testing netlify auto deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { PageHeader } from 'react-bootstrap';
 
 const Header = () => (    
-    <PageHeader>Country Resort Bus Schedule <small>By Nick Gray</small></PageHeader>
+    <PageHeader>Country Resort Bus Scheduler <small>By Nick Gray</small></PageHeader>
 )
 
 export default Header;
